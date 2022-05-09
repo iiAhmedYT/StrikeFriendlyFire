ed3f52b336ad07790c14e0f1f7fcde595ceb366f
iiAhmedYT <61851106+iiAhmedYT@users.noreply.github.com>
2022-05-09 02:18:54 +0200

    2 lines less

--- src/main/java/dev/iiahmed/strikefriendlyfire/StrikeFriendlyFire.java
+++ src/main/java/dev/iiahmed/strikefriendlyfire/StrikeFriendlyFire.java
@@ -29,14 +29,12 @@ public final class StrikeFriendlyFire extends JavaPlugin {
         Fight firstFight = api.getFight(one);
         Fight secondFight= api.getFight(two);
         if(firstFight == null) return false;
-        if(firstFight.equals(secondFight)){
-            boolean ffa = firstFight.getArena().isFFA();
-            if(!ffa) return false;
-            Party party = api.getParty(one);
-            if(party == null) return false;
-            return party.getPlayers().contains(two);
-        }
-        return false;
+        if(!firstFight.equals(secondFight)) return false;
+        boolean ffa = firstFight.getArena().isFFA();
+        if(!ffa) return false;
+        Party party = api.getParty(one);
+        if(party == null) return false;
+        return party.getPlayers().contains(two);
     }
 
 }
